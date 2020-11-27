59b208c83a4a3154b8d64b47bed6ee4d0ee88d89
H350902 <alexandros.kolydas@honeywell.com>
2020-11-27 11:42:00 +0100

    fixed path and insert IEnumerable value to method

--- 2018/AdventOfCode2018/AdventOfCode2018/C01/Challenge01.cs
+++ 2018/AdventOfCode2018/AdventOfCode2018/C01/Challenge01.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -8,30 +9,24 @@ namespace AdventOfCode2018.C01
 {
     public static class Challenge01
     {
+        public static IEnumerable<int> allValues { get; private set; }
+
         public static void Run()
         {
-            string path;
-            path= Path.Combine("C:\\Users\\H350902\\source\\advent-of-code\\2018\\AdventOfCode2018\\AdventOfCode2018\\C01\\FrequencyChanges.txt");
-            GetFrequencyValues(path);
+            allValues = GetFrequencyValues("C01/FrequencyChanges.txt");
+
+            foreach(var i in allValues)
+            {
+                Console.WriteLine(i);
+            }
         }
 
 
-        public static List<int> GetFrequencyValues(string path)
+        public static IEnumerable<int> GetFrequencyValues(string path)
         {
-            List<int> freqNumbers = new List<int>();
-            int[] numbers;
-            foreach( string line in File.ReadLines(path))
-            {
-                //Get the numbers from each line using LINQ
-                numbers = line.Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
 
-                foreach(var num in numbers)
-                {
-                    freqNumbers.Add(num);
-                }
-
-            }
-            return freqNumbers;
+            IEnumerable<int> freqValues =File.ReadLines(path).Select(line=> Convert.ToInt32(line)).ToList(); //https://stackoverflow.com/questions/28859074/read-all-lines-in-text-file-with-separator
+            return freqValues;
         }
     }
 }
