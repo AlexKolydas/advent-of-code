6db5da2e89a59dc1103de4b25b25c9c362b08b9a
H350902 <alexandros.kolydas@honeywell.com>
2020-11-27 12:48:38 +0100

    Beautify Code

--- 2018/AdventOfCode2018/AdventOfCode2018/C01/Challenge01.cs
+++ 2018/AdventOfCode2018/AdventOfCode2018/C01/Challenge01.cs
@@ -9,24 +9,21 @@ namespace AdventOfCode2018.C01
 {
     public static class Challenge01
     {
-        public static IEnumerable<int> allValues { get; private set; }
-
         public static void Run()
         {
-            allValues = GetFrequencyValues("C01/FrequencyChanges.txt");
+            IEnumerable<int> allValues = GetFrequencyValues("C01/FrequencyChanges.txt");
 
             foreach(var i in allValues)
-            {
                 Console.WriteLine(i);
-            }
         }
 
 
         public static IEnumerable<int> GetFrequencyValues(string path)
         {
 
-            IEnumerable<int> freqValues =File.ReadLines(path).Select(line=> Convert.ToInt32(line)).ToList(); //https://stackoverflow.com/questions/28859074/read-all-lines-in-text-file-with-separator
-            return freqValues;
+            //IEnumerable<int> freqValues=File.ReadLines(path).Select(x=> Convert.ToInt32(x)).ToList(); //https://stackoverflow.com/questions/28859074/read-all-lines-in-text-file-with-separator
+            return File.ReadLines(path).Select(x=> Convert.ToInt32(x)); //IEnumerable doesn't need to have ToList()
+
         }
     }
 }
